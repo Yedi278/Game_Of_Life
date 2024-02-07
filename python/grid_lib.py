9e43357d98d75d1c23df31a11bbd6843f084e838
Yehan <y.edirisinghemudi@campus.unimib.it>
2024-02-07 22:35:47 +0100

    .

--- python/grid_lib.py
+++ python/grid_lib.py
@@ -22,3 +22,15 @@ def draw(surface, mat):
                 rec = (r[0]*i, r[1]*j, r[0]-contour[0],r[1]-contour[1])
                 c = np.random.randint(0,255,3)
                 pygame.draw.rect(surface,c,rec)
+
+def get_user_input(surface, mat, pos):
+    surface_shape = surface.get_size()
+    r = ( int(surface_shape[0]/mat.shape[0]),   int(surface_shape[1]/mat.shape[1]))
+
+    for i in range(mat.shape[0]):
+        for j in range(mat.shape[1]):
+
+            if r[0]*i < pos[0] < r[0]*(i+1):
+                if r[1]*j < pos[1] < r[1]*(j+1):
+                    if mat[i,j] == 1: mat[i,j] = 0
+                    elif mat[i,j] == 0: mat[i,j] = 1
--- python/main.py
+++ python/main.py
@@ -8,20 +8,31 @@ pygame.init()
 pygame.display.set_caption("~ Game Of Life ~")
 screen = pygame.display.set_mode(size)
 
-matrix = grid(size, density=4)
+matrix = grid(size, density=10)
 matrix = np.random.randint(0,2, size=matrix.shape)
 
-run = True
+run,start = True,False
 while run:
 
     screen.fill(BLACK)
     draw(screen,matrix)
-    matrix = game(matrix)
+    
+    if start == True:
+        pygame.display.set_caption("~ Game Of Life (Running) ~")
+        matrix = game(matrix)
+    else:
+        pygame.display.set_caption("~ Game Of Life (Pause) ~")
 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: run = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE: run = False
-        
+            if event.key == pygame.K_SPACE: 
+                if start: start = False
+                else: start = True
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            get_user_input(screen,matrix,pos)
+
     pygame.display.flip()
-    pygame.time.delay(250)
+    # pygame.time.delay(100)
